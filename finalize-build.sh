72507438452c400465c1f8b26dc20955b0b11e1d
Aref Akbari <arefrf2010@gmail.com>
2022-03-28 23:02:51 +0430

    finalize build

---
+++ finalize-build.sh
@@ -0,0 +1,11 @@
+mkdir -p lib/style/components/
+sass --style=compressed src/style/index.scss lib/style.css
+for file in lib/**/*.js; do
+	sed -i 's/.module.scss/.module.css/g' "$file"
+	sed -i 's/..\/..\/style\/components/..\/style\/components/g' "$file"
+    uglifyjs "$file" -c -o "$file" 
+done 
+
+for file in src/style/components/*.scss; do
+    sass --style=compressed "$file" "lib/style/components/$(basename "$file" .css)" 
+done 
