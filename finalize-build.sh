a34c93141bc64d53c0193cd3f2cc5b737c02cb6f
Aref Akbari <arefrf2010@gmail.com>
2022-03-28 23:10:25 +0430

    fixed style path

--- finalize-build.sh
+++ finalize-build.sh
@@ -2,7 +2,7 @@ mkdir -p lib/style/components/
 sass --style=compressed src/style/index.scss lib/style.css
 for file in lib/**/*.js; do
 	sed -i 's/.module.scss/.module.css/g' "$file"
-	sed -i 's/..\/..\/style\/components/..\/style\/components/g' "$file"
+	sed -i 's/..\/..\/style\/components/..\/style/g' "$file"
     uglifyjs "$file" -c -o "$file" 
 done 
 
