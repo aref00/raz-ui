fe6d035b5a4994129b1340d36853381c7bd8f333
Aref Akbari <arefrf2010@gmail.com>
2022-03-29 00:09:14 +0430

    finalize changes

--- finalize-build.sh
+++ finalize-build.sh
@@ -1,11 +1,17 @@
-mkdir -p lib/style/components/
+mkdir -p lib/style/
 sass --style=compressed src/style/index.scss lib/style.css
+for file in lib/*.js; do
+    uglifyjs "$file" --compress -o "$file" 
+done 
 for file in lib/**/*.js; do
+    uglifyjs "$file" --compress -o "$file" 
+done 
+for file in lib/**/**/*.js; do
 	sed -i 's/.module.scss/.module.css/g' "$file"
-	sed -i 's/..\/..\/style\/components/..\/style/g' "$file"
-    uglifyjs "$file" -c -o "$file" 
+	# sed -i 's/..\/..\/style\/components/..\/style/g' "$file"
+    uglifyjs "$file" --compress -o "$file" 
 done 
 
 for file in src/style/components/*.scss; do
-    sass --style=compressed "$file" "lib/style/components/$(basename "$file" .css)" 
+    sass --style=compressed "$file" "lib/style/components/$(basename "$file" .scss).css" 
 done 
