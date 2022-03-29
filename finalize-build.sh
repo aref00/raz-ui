f0d9d4dc0c7b317e5fc59a13998a943a214dc54d
Aref Akbari <arefrf2010@gmail.com>
2022-03-29 10:03:45 +0430

    shared/components styling

--- finalize-build.sh
+++ finalize-build.sh
@@ -1,17 +1,5 @@
-mkdir -p lib/style/
 sass --style=compressed src/style/index.scss lib/style.css
-for file in lib/*.js; do
-    uglifyjs "$file" --compress -o "$file" 
-done 
-for file in lib/**/*.js; do
-    uglifyjs "$file" --compress -o "$file" 
-done 
-for file in lib/**/**/*.js; do
-	sed -i 's/.scss/.css/g' "$file"
-	# sed -i 's/..\/..\/style\/components/..\/style/g' "$file"
-    uglifyjs "$file" --compress -o "$file" 
-done 
 
 for file in src/style/components/*.scss; do
-    sass --style=compressed "$file" "lib/style/components/$(basename "$file" .scss).css" 
+    sass --style=compressed "$file" "lib/$(basename "$file" .scss).css" 
 done 
